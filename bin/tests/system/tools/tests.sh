917e91b84e25e8e9cbf196f9eda2d527b417f042
Petr Špaček <pspacek@isc.org>
2025-07-01 11:12:15 +0200

    Test command line tools without stdio fds
    Testing all combinations seems unnecessary but is cheap.
    I was too lazy to run this against all tools we have. nsupdate was
    chosen because it is one of few tools which actually use stdin and the
    original issue was reproducible even without any network communication,
    which was not the case for simple dig invocation.
    
    Sorry for new shell test but doing this in Python seemed very
    complicated and fragile.
    
    (cherry picked from commit e415c5c996a3b408eb1064bd86048522897fc811)

---
+++ bin/tests/system/tools/tests.sh
@@ -0,0 +1,24 @@
+#!/usr/sh
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+set -e
+
+. ../conf.sh
+
+echo_i "tools must not crash if stdio is closed"
+"$NSUPDATE" 0>&-
+"$NSUPDATE" 0>&- 1>&-
+"$NSUPDATE" 0>&- 1>&- 2>&-
+"$NSUPDATE" 0>&- 2>&-
+"$NSUPDATE" 1>&-
+"$NSUPDATE" 1>&- 2>&-
+"$NSUPDATE" 2>&-
