c756b8a505ca1975e967f59477370287077c1f12
Matthijs Mekking <matthijs@isc.org>
2025-07-31 17:41:11 +0200

    Fix autosign system test
    When creating keys, set Publish and Activate times so that keys will
    be initialized as omnipresent. This way we start with a safe DNSSEC
    state. In most cases at least, because some tests depend on special
    key timings.
    
    The ttl[1-4].example cases have become incorrect. With dnssec-policy
    we require the TTL to match the dnskey-ttl from the policy.
    
    The delzsk.example will have a ZSK removed from the zone. It also
    requires that the DNSKEY RRset is already published. This means
    that for the existing keys the, no longer "is now published"
    messages will be logged.
    
    The nsec-only.example and reconf.example zones are fixed to have a
    correct matching policy.
    
    This all means the expected count of log messages changes slightly.

--- bin/tests/system/autosign/setup.sh
+++ bin/tests/system/autosign/setup.sh
@@ -15,6 +15,11 @@
 
 copy_setports ns1/named.conf.in ns1/named.conf
 copy_setports ns2/named.conf.in ns2/named.conf
+if [ $RSASHA1_SUPPORTED = 1 ]; then
+  cp ns3/nsec-only.conf.in ns3/nsec-only.conf
+else
+  : >ns3/nsec-only.conf
+fi
 copy_setports ns3/named.conf.in ns3/named.conf
 copy_setports ns4/named.conf.in ns4/named.conf
 copy_setports ns5/named.conf.in ns5/named.conf
