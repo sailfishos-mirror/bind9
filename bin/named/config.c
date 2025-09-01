cae623fa690f9de0600d7d9a207e198be1920949
Michał Kępień <michal@isc.org>
2025-09-01 21:35:33 +0200

    Obsolete the "tkey-domain" statement
    The "tkey-domain" statement has effectively been a no-op since commit
    bd4576b3cef88bcb78ae0dd7619019be4fdfb2ea, which removed the only bit of
    code using it: the logic implementing TKEY Mode 2 (Diffie-Hellman).
    
    A subsequent cleanup commit, 885c132f4ae6d895ee973648e15d9e61d01555a8,
    also missed the opportunity to remove the "tkey-domain" statement
    altogether.
    
    Mark the "tkey-domain" statement as obsolete and remove all code and
    documentation related to it.
    
    (cherry picked from commit 805f1c0f6539dae29d11799af2de4f8ab2a02a98)

--- bin/named/config.c
+++ bin/named/config.c
@@ -123,7 +123,6 @@ options {\n\
 	tcp-listen-queue 10;\n\
 	tcp-receive-buffer 0;\n\
 	tcp-send-buffer 0;\n\
-#	tkey-domain <none>\n\
 #	tkey-gssapi-credential <none>\n\
 	transfer-message-size 20480;\n\
 	transfers-in 10;\n\
