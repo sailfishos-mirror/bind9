019e70db4dbd20c8fd14caa56926919b289a0ecf
Alessio Podda <alessio@isc.org>
2025-10-07 06:00:17 +0200

    Move setresign to rdataset.c and rename it
    The setresign method is not diff specific, it only returns the minimum
    resign time of an rdataset. Move it to rdataset.c to simplify late
    refactoring.
    
    (cherry picked from commit 6f726ae3db00e98f063302363b39fa41a5a7a8d3)

--- lib/dns/include/dns/rdataset.h
+++ lib/dns/include/dns/rdataset.h
@@ -691,4 +691,17 @@ dns_trust_totext(dns_trust_t trust);
  * Display trust in textual form.
  */
 
+isc_stdtime_t
+dns_rdataset_minresign(dns_rdataset_t *rdataset);
+/*%<
+ * Return the minimum resign time from an RRSIG rdataset.
+ *
+ * This function iterates through all RRSIG records in the rdataset
+ * and returns the earliest expiration time, which indicates when
+ * the signatures should be resigned.
+ *
+ * Requires:
+ * \li	'rdataset' is a valid rdataset.
+ */
+
 ISC_LANG_ENDDECLS
