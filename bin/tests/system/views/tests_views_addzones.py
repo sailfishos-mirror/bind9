e50edc909078ac6279d8b6e6d621ab16140f6c01
Colin Vidal <colin@isc.org>
2025-11-07 10:45:09 +0100

    rewrite views/addzone in loop system test
    A part of the `views` system test attempts to add multiples zones in a
    loop, and after each zone being added, reconfig the server.
    
    However, the test didn't take into account the fact that the server
    might take a bit more time to reload than the script to move to the next
    iteration, and in some case the test was re-requesting the server reload
    when it was still reloading.
    
    Since `b49f83a3`, `named` explicitly fails to reload when a load/reload
    is pending, which is (unless proved otherwise) the reason of the test
    was now randomly failing.
    
    That part of the test is now waiting for the server log message saying
    the server has added the new zone and is running. Also, that part of the
    test has been rewrote in Python.

---
+++ bin/tests/system/views/tests_views_addzones.py
@@ -0,0 +1,25 @@
+# Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# See the COPYRIGHT file distributed with this work for additional
+# information regarding copyright ownership.
+
+import shutil
+
+
+def test_views_add_zones(ns2, templates):
+    zone_names = []
+    for i in range(50):
+        name = f"example{i:03}.com"
+        zone_names.append(name)
+        templates.render("ns2/named.conf", {"zone_names": zone_names})
+        shutil.copyfile("ns2/zone.db.in", f"ns2/{name}.db")
+        with ns2.watch_log_from_here() as watcher:
+            ns2.rndc("reconfig", log=False)
+            log_seq = ["any newly configured zones are now loaded", "running"]
+            watcher.wait_for_sequence(log_seq)
