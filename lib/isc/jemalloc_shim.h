95cc515e2045772fcdf2286770ec8053f307930d
Ondřej Surý <ondrej@isc.org>
2025-11-24 10:06:51 +0100

    Provide more information when the memory allocation fails
    Instead of just crashing when memory allocation fails, also print a
    message saying "Out of memory!", the size of the allocation that failed,
    total allocated memory from all memory contexts and value of errno.
    
    (cherry picked from commit b0194004d9de54a3be5652ad4eb7ba9b48b11eee)

--- lib/isc/jemalloc_shim.h
+++ lib/isc/jemalloc_shim.h
@@ -39,7 +39,9 @@ mallocx(size_t size, int flags) {
 
 	size_t bytes = ISC_CHECKED_ADD(size, sizeof(size_info));
 	size_info *si = malloc(bytes);
-	INSIST(si != NULL);
+	if (si == NULL) {
+		return NULL;
+	}
 
 	si->size = size;
 	ptr = &si[1];
@@ -67,8 +69,11 @@ sallocx(void *ptr, int flags ISC_ATTR_UNUSED) {
 
 static inline void *
 rallocx(void *ptr, size_t size, int flags) {
-	size_info *si = realloc(&(((size_info *)ptr)[-1]), size + sizeof(*si));
-	INSIST(si != NULL);
+	size_t bytes = ISC_CHECKED_ADD(size, sizeof(size_info));
+	size_info *si = realloc(&(((size_info *)ptr)[-1]), bytes);
+	if (si == NULL) {
+		return NULL;
+	}
 
 	if ((flags & MALLOCX_ZERO) != 0 && size > si->size) {
 		memset((uint8_t *)si + sizeof(*si) + si->size, 0,
