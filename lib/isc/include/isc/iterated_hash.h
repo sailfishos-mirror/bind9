3801d0ebbf8da69077af84dae7f7ec23718b839b
Mark Andrews <marka@isc.org>
2026-02-18 12:30:22 +1100

    Enforce NSEC3 record consistency
    NSEC3 hashes are required to fit within a single DNS label.  Since there
    are 5 bits per label byte without pad characters, the maximum hash size
    is floor(63*5/8) (39 bytes).
    
    This patch enforces this maximum length for unknown algorithms, while
    strictly enforcing the exact expected digest length for known algorithms
    like SHA-1.

--- lib/isc/include/isc/iterated_hash.h
+++ lib/isc/include/isc/iterated_hash.h
@@ -13,18 +13,6 @@
 
 #pragma once
 
-/*
- * The maximal hash length that can be encoded in a name
- * using base32hex.  floor(255/8)*5
- */
-#define NSEC3_MAX_HASH_LENGTH 155
-
-/*
- * The maximum has that can be encoded in a single label using
- * base32hex.  floor(63/8)*5
- */
-#define NSEC3_MAX_LABEL_HASH 35
-
 int
 isc_iterated_hash(unsigned char *out, const unsigned int hashalg,
 		  const int iterations, const unsigned char *salt,
