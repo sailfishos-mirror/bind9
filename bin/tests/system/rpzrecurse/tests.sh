ae67c1851dcd4ce3e03012f7a6ac6cfa0eb6b37f
Evan Hunt <each@isc.org>
2026-03-21 20:28:24 -0700

    rpz_rrset_find() now recurses on ISC_R_NOTFOUND
    previously, rpz_rrset_find() behaved differently depending on whether
    a cache lookup returned DNS_R_DELEGATION or ISC_R_NOTFOUND.  the former
    indicates the presence of a cached NS rrset, and the latter indicates
    that the cache is cold or that all NS rrsets above the query name have
    expired. both results indicate that the caller should recurse, but
    rpz_rrset_find() only recursed in the case of DNS_R_DELEGATION.
    
    the nsip-wait-recurse and nsdname-wait-recurse test cases in the
    rpzrecurse system test were dependent on this misbehavior. the test
    server was configured with a lame delegation, so that recursion always
    failed, but once the lame delegation was expired due to a zero TTL, the
    cache returned ISC_R_NOTFOUND, which caused the recursion not to be
    attempted. the test seemed to be observing a delay before recursion
    succeeded, but it was actually observing a delay before recursion was
    skipped. fixing this bug caused the test to fail.
    
    the test server has now been reconfigured so that recursion succeeds
    after a delay, instead of failing. now we're able to test that
    we're waiting for the successful completion of recursion.

--- bin/tests/system/rpzrecurse/tests.sh
+++ bin/tests/system/rpzrecurse/tests.sh
@@ -442,7 +442,7 @@ add_test_marker 10.53.0.2 10.53.0.3
 echo_i "timing 'nsip-wait-recurse yes' (default)"
 ret=0
 t1=$($PERL -e 'print time()."\n";')
-$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.yes.$t
+$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.yes.$t || ret=1
 t2=$($PERL -e 'print time()."\n";')
 p1=$((t2 - t1))
 echo_i "elapsed time $p1 seconds"
@@ -455,7 +455,7 @@ wait_for_log 20 "rpz: policy: reload done" ns3/named.run || ret=1
 
 echo_i "timing 'nsip-wait-recurse no'"
 t3=$($PERL -e 'print time()."\n";')
-$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.no.$t
+$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.no.$t || ret=1
 t4=$($PERL -e 'print time()."\n";')
 p2=$((t4 - t3))
 echo_i "elapsed time $p2 seconds"
@@ -477,7 +477,7 @@ add_test_marker 10.53.0.2 10.53.0.3
 echo_i "timing 'nsdname-wait-recurse yes' (default)"
 ret=0
 t1=$($PERL -e 'print time()."\n";')
-$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.yes.$t
+$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.yes.$t || ret=1
 t2=$($PERL -e 'print time()."\n";')
 p1=$((t2 - t1))
 echo_i "elapsed time $p1 seconds"
@@ -490,7 +490,7 @@ wait_for_log 20 "rpz: policy: reload done" ns3/named.run || ret=1
 
 echo_i "timing 'nsdname-wait-recurse no'"
 t3=$($PERL -e 'print time()."\n";')
-$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.no.$t
+$DIG -p ${PORT} @10.53.0.3 foo.child.example.tld a >dig.out.no.$t || ret=1
 t4=$($PERL -e 'print time()."\n";')
 p2=$((t4 - t3))
 echo_i "elapsed time $p2 seconds"
